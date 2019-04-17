6a13b58643fe9e838f1bd786c052fd93cb011e72
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-04-17 14:01:38 +0200

    move data files into lmp directory

--- train_nnp.sh
+++ train_nnp.sh
@@ -2,37 +2,35 @@
 
 clear
 
-rm -f *.out *.data
 echo "Generating initial airebo dataset..."
+rm -f lmp/*.out lmp/*.data
 lmp_serial < md.airebo.in #> md.airebo.out
-cp airebo.data airebo0.data
+cp lmp/airebo.data lmp/airebo0.data
 # ----------------------------------------
-#rm -f *.out 
-#cp airebo0.data airebo.data
+#rm -f lmp/*.out 
+#cp lmp/airebo0.data lmp/airebo.data
 
 
-for i in $(seq 1 1 1)
+for i in $(seq 1 1 3)
 do
+	echo "---------------------------"
 	echo "Extending dataset... (step $i)"
 
-	python lammps_to_runner.py airebo.data input.data
+	python lammps_to_runner.py lmp/airebo.data lmp/input.data
 
 	echo "Using RuNNer..."
 	cd nnp
 	rm -f *.out *.data
-	mv ../input.data .
-	sh runscript.sh | tee runscript.out
+	mv ../lmp/input.data .
+	sh runscript.sh #| tee runscript.out
 	cd ..
 
-	rm -f nnp.data
-
-	echo "Running NNP-MD..."
+	echo "Running MD using NNP..."
+	rm -f lmp/nnp.data
 	lmp_serial < md.nnp.in #> md.nnp.$i.out
 
 	echo "Rerunning Airebo..."
 	lmp_serial < rerun.airebo.in #> rerun.airebo.$i.out
-
-	echo "---------------------------"
   
 done
 
